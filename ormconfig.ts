2e71ce2bff18138656a1520e6531b7a9a6c1bfcc
gelosde <angelokvieira@gmail.com>
2022-08-24 19:31:08 -0300

    feat base pack finish

---
+++ ormconfig.ts
@@ -0,0 +1,31 @@
+import dotenv = require("dotenv");
+import path = require("path");
+dotenv.config();
+
+const dbConfig = {
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  logging: false,
+  entities: [path.join(__dirname, "./src/entities/**/*.ts")],
+  migrations: [path.join(__dirname, "./src/migrations/**/*.ts")],
+  cli: {
+    entitiesDir: path.join(__dirname, "./src/entities"),
+    migrationsDir: path.join(__dirname, "./src/migrations"),
+  },
+};
+
+const dbProductionConfig = {
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  logging: false,
+  entities: [path.join(__dirname, "./build/src/entities/**/*.js")],
+  migrations: [path.join(__dirname, "./build/src/migrations/**/*.js")],
+  cli: {
+    entitiesDir: path.join(__dirname, "./build/src/entities"),
+    migrationsDir: path.join(__dirname, "./build/src/migrations"),
+  },
+  ssl: { rejectUnauthorized: false },
+};
+
+module.exports =
+  process.env.NODE_ENV === "production" ? dbProductionConfig : dbConfig;
